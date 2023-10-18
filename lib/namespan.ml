99ef079c0f725c7f8ff6851f4467ed1666a14e40
Guilhem Jaber <guilhem.jaber@inria.fr>
2023-10-18 10:28:48 +0200

    Add id_nspan function

--- lib/namespan.ml
+++ lib/namespan.ml
@@ -1,4 +1,5 @@
 type 'a namespan = ('a,'a) Pmap.pmap
 let empty_nspan = Pmap.empty
+let id_nspan l = Pmap.list_to_pmap @@ List.map (fun x -> (x,x)) l 
 let add_nspan = Pmap.add_span
 let is_in_dom_im = Pmap.is_in_dom_im
