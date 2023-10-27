5fc107b7452e262f7242d9412a0f16e6b50b82d8
Guilhem Jaber <guilhem.jaber@inria.fr>
2023-10-27 19:52:17 +0200

    Add string_of_span to namespan

--- lib/util/namespan.ml
+++ lib/util/namespan.ml
@@ -3,3 +3,4 @@ let empty_nspan = Pmap.empty
 let id_nspan l = Pmap.list_to_pmap @@ List.map (fun x -> (x,x)) l 
 let add_nspan = Pmap.add_span
 let is_in_dom_im = Pmap.is_in_dom_im
+let string_of_span string_of_name nspan =  Pmap.string_of_pmap "" "," string_of_name string_of_name nspan
