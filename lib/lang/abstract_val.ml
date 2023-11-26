443e80e2c2fc12fa3652583c88c207654fd9c099
Guilhem Jaber <guilhem.jaber@inria.fr>
2023-11-26 16:13:15 +0100

    Rework comments about abstract values

--- lib/lang/abstract_val.ml
+++ lib/lang/abstract_val.ml
@@ -2,11 +2,14 @@ module type AVAL = sig
   (*To be instantiated*)
   type name
   type value
+    (* The values filling the holes of abstracted values are negative values *)
   type negative_val
   type typ
+    (* The names appearing in abstracted values are types by negative types *)
   type negative_type
   type typevar
   type name_ctx = (name, negative_type) Util.Pmap.pmap
+    (* Interactive environments γ are partial maps from names to interactive values*)
   type interactive_env = (name, negative_val) Util.Pmap.pmap
   (* *)
 
@@ -18,23 +21,11 @@ module type AVAL = sig
   val string_of_abstract_val : abstract_val -> string
   val names_of_abstract_val : abstract_val -> name list
 
-  (* The names appearing in abstracted values are types by negative types *)
-
-  (*Interactive name contexts are typing contexts mapping names
-     to negative types.*)
-
-  (**  TODO: This should be moved elswehere **)
-
-  (* The values filling the holes of abstracted values are negative values *)
-
-  (* Interactive environments γ are partial maps from names to interactive values*)
-
-  (*val embed_value_env : (name,value) Util.Pmap.pmap -> interactive_env*)
-
-  (* The typed focusing process implemented by abstract_value
-     decomposes typed glue values into:
-      - an abstract value for the observable part,
-      - a typed interactive environment for the negative part. *)
+  (* The typed focusing process implemented by abstracting_value
+     decomposes typed values (V,τ) into:
+      - an abstract value A for the observable part,
+      - a typed interactive environment (γ,Γ) for the negative part.
+    The type τ is needed to guide this abstracting process for polymorphic languages. *)
   val abstracting_value :
     value -> typ -> abstract_val * interactive_env * name_ctx
 
@@ -48,12 +39,12 @@ module type AVAL = sig
   val type_check_abstract_val :
     name_ctx -> name_ctx -> typ -> abstract_val -> name_ctx option
 
-  (* From the interactive name context Γ_P and a type τ,
+  module M : Util.Monad.BRANCH
+
+    (* From the interactive name context Γ_P and a type τ,
      we generate all the possible pairs (A,Δ) such that
      Γ_P;_ ⊢ A : τ ▷ Δ
      Freshness of names that appear in Δ is guaranteed by a gensym, so that we do not need to provide Γ_O. *)
-  module M : Util.Monad.BRANCH
-
   val generate_abstract_val : name_ctx -> typ -> (abstract_val * name_ctx) M.m
 
   val unify_abstract_val :
