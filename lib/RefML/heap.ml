5e0ec7aa71c26295b1fc9a0f38e5e93cce511b07
Guilhem Jaber <guilhem.jaber@inria.fr>
2023-11-26 15:01:15 +0100

    Cleanup and extension of heap.ml

--- lib/RefML/heap.ml
+++ lib/RefML/heap.ml
@@ -1,6 +1,6 @@
 (* Concrete Heaps *)
 
-type heap = (Syntax.loc, Syntax.term) Util.Pmap.pmap
+type heap = (Syntax.loc, Syntax.value) Util.Pmap.pmap
 
 let emptyheap = Util.Pmap.empty
 
@@ -9,10 +9,14 @@ let allocate heap v =
   (l, Util.Pmap.add (l, v) heap)
 
 let modify heap l value = Util.Pmap.modadd (l, value) heap
+let update heap heap' =
+  Util.Pmap.fold (fun heap (l,value) -> modify heap l value) heap heap'
+
 let access heap l = Util.Pmap.lookup l heap
 
 let string_of_heap =
-  Util.Pmap.string_of_pmap "ε" "↪" Syntax.string_of_loc Syntax.string_of_term
+  Util.Pmap.string_of_pmap "[]" "->" Syntax.string_of_loc Syntax.string_of_term
+
 
 let loc_ctx_of_heap = Util.Pmap.map_im (fun _ -> Types.TInt)
 
@@ -24,6 +28,7 @@ let rec shuffle_heaps = function
       List.flatten (List.map aux listval)
 
 let generate_heaps loc_ctx =
+  Util.Debug.print_debug @@ "Generating heap for " ^ Type_ctx.string_of_loc_ctx loc_ctx;
   let list_predheap =
     Util.Pmap.map_list
       (fun (l, ty) -> (l, Syntax.generate_ground_value ty))
--- lib/RefML/heap.mli
+++ lib/RefML/heap.mli
@@ -1,9 +1,10 @@
-type heap = (Syntax.loc, Syntax.term) Util.Pmap.pmap
+type heap = (Syntax.loc, Syntax.value) Util.Pmap.pmap
 
 val emptyheap : heap
-val allocate : heap -> Syntax.term -> Syntax.loc * heap
-val modify : heap -> Syntax.loc -> Syntax.term -> heap
-val access : heap -> Syntax.loc -> Syntax.term option
+val allocate : heap -> Syntax.value -> Syntax.loc * heap
+val modify : heap -> Syntax.loc -> Syntax.value -> heap
+val update : heap -> heap -> heap
+val access : heap -> Syntax.loc -> Syntax.value option
 val string_of_heap : heap -> string
 val loc_ctx_of_heap : heap -> Type_ctx.loc_ctx
 val generate_heaps : Type_ctx.loc_ctx -> heap list
