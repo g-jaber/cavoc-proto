007ebda0cf861adb2f26d5c80510be4368410b35
Guilhem Jaber <guilhem.jaber@gmail.com>
2025-08-04 18:10:51 +0200

    Remove last bits of Actions

--- lib/lts/typelts.ml
+++ lib/lts/typelts.ml
@@ -3,10 +3,9 @@ module type INT = sig
   module IntLang : Lang.Interactive.LANG
   module Name : Lang.Names.CONT_NAMES with type name = IntLang.Name.name
 
-  module Actions :
-    Actions.ACTIONS
-      with type Moves.name = IntLang.Name.name
-       and type Moves.kdata = IntLang.abstract_normal_form
+  module Moves : Moves.MOVES
+      with type name = IntLang.Name.name
+       and type kdata = IntLang.abstract_normal_form
   (* *)
 
   type type_ctx
@@ -15,6 +14,6 @@ module type INT = sig
     IntLang.Store.store_ctx -> IntLang.name_ctx -> IntLang.name_ctx -> type_ctx
 
   val string_of_type_ctx : type_ctx -> string
-  val generate__moves : type_ctx -> (Actions.Moves.move * type_ctx) IntLang.M.m
-  val check_input_move : type_ctx -> Actions.Moves.move -> type_ctx option
+  val generate__moves : type_ctx -> (Moves.move * type_ctx) IntLang.M.m
+  val check_input_move : type_ctx -> Moves.move -> type_ctx option
 end
