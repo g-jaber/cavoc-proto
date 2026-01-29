9062fbda96a496f702cd8954d24760ae5cb245ae
Guilhem Jaber <guilhem.jaber@gmail.com>
2026-01-29 14:08:52 +0100

    Correct type checking of assert

--- lib/RefML/type_checker.ml
+++ lib/RefML/type_checker.ml
@@ -23,15 +23,13 @@ let rec infer_type type_ctx type_subst expr =
       | Some (TArrow (pty, ty)) ->
           let (pty', type_subst') = infer_type type_ctx type_subst e in
           (*Should we instantiate pty' ? *)
-          begin
-            match mgu_type (Type_ctx.get_type_env type_ctx) (pty, pty') with
-            | Some type_subst'' ->
-                (ty, compose_type_subst type_subst'' type_subst')
-            | None ->
-                Util.Error.fail_error
-                  ("Error typing " ^ Syntax.string_of_term e ^ ": "
-                 ^ string_of_typ pty' ^ " is not equal to " ^ string_of_typ pty
-                  )
+          begin match mgu_type (Type_ctx.get_type_env type_ctx) (pty, pty') with
+          | Some type_subst'' ->
+              (ty, compose_type_subst type_subst'' type_subst')
+          | None ->
+              Util.Error.fail_error
+                ("Error typing " ^ Syntax.string_of_term e ^ ": "
+               ^ string_of_typ pty' ^ " is not equal to " ^ string_of_typ pty)
           end
       | Some cty ->
           Util.Error.fail_error
@@ -99,13 +97,12 @@ let rec infer_type type_ctx type_subst expr =
       let (ty2, type_subst2) = infer_type type_ctx type_subst1 e2 in
       let (ty3, type_subst3) = infer_type type_ctx type_subst2 e3 in
       (* Should we instantiate ty2 and ty3 ?*)
-      begin
-        match mgu_type (Type_ctx.get_type_env type_ctx) (ty2, ty3) with
-        | Some type_subst' -> (ty3, compose_type_subst type_subst' type_subst3)
-        | None ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ ": "
-             ^ string_of_typ ty2 ^ " is not equal to " ^ string_of_typ ty3)
+      begin match mgu_type (Type_ctx.get_type_env type_ctx) (ty2, ty3) with
+      | Some type_subst' -> (ty3, compose_type_subst type_subst' type_subst3)
+      | None ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ ": "
+           ^ string_of_typ ty2 ^ " is not equal to " ^ string_of_typ ty3)
       end
   | Fun ((var, TUndef), e) ->
       let tvar = fresh_typevar () in
@@ -125,14 +122,13 @@ let rec infer_type type_ctx type_subst expr =
       let type_ctx'' =
         Type_ctx.extend_var_ctx type_ctx' idfun (TArrow (tvar1, tvar2)) in
       let (rty, type_subst') = infer_type type_ctx'' type_subst e in
-      begin
-        match mgu_type (Type_ctx.get_type_env type_ctx) (tvar2, rty) with
-        | Some type_subst'' ->
-            (TArrow (tvar1, rty), compose_type_subst type_subst'' type_subst')
-        | None ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ ": "
-             ^ string_of_typ tvar2 ^ " is not equal to " ^ string_of_typ rty)
+      begin match mgu_type (Type_ctx.get_type_env type_ctx) (tvar2, rty) with
+      | Some type_subst'' ->
+          (TArrow (tvar1, rty), compose_type_subst type_subst'' type_subst')
+      | None ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ ": "
+           ^ string_of_typ tvar2 ^ " is not equal to " ^ string_of_typ rty)
       end
   | Fix ((idfun, TUndef), (var, aty), e) ->
       let tvar2 = fresh_typevar () in
@@ -140,40 +136,38 @@ let rec infer_type type_ctx type_subst expr =
       let type_ctx' =
         Type_ctx.extend_var_ctx type_ctx' idfun (TArrow (aty, tvar2)) in
       let (rty, type_subst') = infer_type type_ctx' type_subst e in
-      begin
-        match Util.Pmap.lookup idfun type_ctx'.var_ctx with
-        | Some fty -> begin
-            match
-              mgu_type (Type_ctx.get_type_env type_ctx) (fty, TArrow (aty, rty))
-            with
-            | Some type_subst'' ->
-                (TArrow (aty, rty), compose_type_subst type_subst'' type_subst')
-            | None ->
-                Util.Error.fail_error
-                  ("Error typing " ^ Syntax.string_of_term expr ^ ": "
-                 ^ string_of_typ fty ^ " is not equal to "
-                  ^ string_of_typ (TArrow (aty, rty)))
-          end
-        | None ->
-            failwith
-              ("Variable " ^ Syntax.string_of_id idfun
-             ^ " not found type-checking. Please report.")
+      begin match Util.Pmap.lookup idfun type_ctx'.var_ctx with
+      | Some fty -> begin
+          match
+            mgu_type (Type_ctx.get_type_env type_ctx) (fty, TArrow (aty, rty))
+          with
+          | Some type_subst'' ->
+              (TArrow (aty, rty), compose_type_subst type_subst'' type_subst')
+          | None ->
+              Util.Error.fail_error
+                ("Error typing " ^ Syntax.string_of_term expr ^ ": "
+               ^ string_of_typ fty ^ " is not equal to "
+                ^ string_of_typ (TArrow (aty, rty)))
+        end
+      | None ->
+          failwith
+            ("Variable " ^ Syntax.string_of_id idfun
+           ^ " not found type-checking. Please report.")
       end
   | Fix ((idfun, fty), (var, aty), e) ->
       let type_ctx' = Type_ctx.extend_var_ctx type_ctx var aty in
       let type_ctx' = Type_ctx.extend_var_ctx type_ctx' idfun fty in
       let (rty, type_subst') = infer_type type_ctx' type_subst e in
-      begin
-        match
-          mgu_type (Type_ctx.get_type_env type_ctx) (fty, TArrow (aty, rty))
-        with
-        | Some type_subst'' ->
-            (TArrow (aty, rty), compose_type_subst type_subst'' type_subst')
-        | None ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ ": "
-             ^ string_of_typ fty ^ " is not equal to "
-              ^ string_of_typ (TArrow (aty, rty)))
+      begin match
+        mgu_type (Type_ctx.get_type_env type_ctx) (fty, TArrow (aty, rty))
+      with
+      | Some type_subst'' ->
+          (TArrow (aty, rty), compose_type_subst type_subst'' type_subst')
+      | None ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ ": "
+           ^ string_of_typ fty ^ " is not equal to "
+            ^ string_of_typ (TArrow (aty, rty)))
       end
   | Let (var, e1, e2) ->
       let (ty, type_subst') = infer_type type_ctx type_subst e1 in
@@ -192,17 +186,16 @@ let rec infer_type type_ctx type_subst expr =
       let tvar2 = fresh_typevar () in
       let type_ctx' = Type_ctx.extend_var_ctx type_ctx var1 tvar1 in
       let type_ctx'' = Type_ctx.extend_var_ctx type_ctx' var2 tvar2 in
-      begin
-        match
-          mgu_type (Type_ctx.get_type_env type_ctx) (ty, TProd (tvar1, tvar2))
-        with
-        | Some type_subst'' ->
-            let type_subst'' = compose_type_subst type_subst'' type_subst' in
-            infer_type type_ctx'' type_subst'' e2
-        | None ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ " : "
-             ^ string_of_typ ty ^ " is not a product type")
+      begin match
+        mgu_type (Type_ctx.get_type_env type_ctx) (ty, TProd (tvar1, tvar2))
+      with
+      | Some type_subst'' ->
+          let type_subst'' = compose_type_subst type_subst'' type_subst' in
+          infer_type type_ctx'' type_subst'' e2
+      | None ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ " : "
+           ^ string_of_typ ty ^ " is not a product type")
       end
   | App (e1, e2) ->
       let (aty, type_subst') = infer_type type_ctx type_subst e2 in
@@ -216,16 +209,15 @@ let rec infer_type type_ctx type_subst expr =
         (Syntax.string_of_term e2 ^ " is of type " ^ string_of_typ aty'
        ^ " (was " ^ string_of_typ aty ^ ")");
       let tvar = fresh_typevar () in
-      begin
-        match
-          mgu_type (Type_ctx.get_type_env type_ctx) (fty', TArrow (aty', tvar))
-        with
-        | Some tsubst''' -> (tvar, compose_type_subst tsubst''' type_subst'')
-        | None ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ ": "
-             ^ string_of_typ fty' ^ " is not equal to "
-              ^ string_of_typ (TArrow (aty', tvar)))
+      begin match
+        mgu_type (Type_ctx.get_type_env type_ctx) (fty', TArrow (aty', tvar))
+      with
+      | Some tsubst''' -> (tvar, compose_type_subst tsubst''' type_subst'')
+      | None ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ ": "
+           ^ string_of_typ fty' ^ " is not equal to "
+            ^ string_of_typ (TArrow (aty', tvar)))
       end
   | Seq (e1, e2) ->
       let type_subst' = check_type type_ctx type_subst e1 TUnit in
@@ -247,74 +239,60 @@ let rec infer_type type_ctx type_subst expr =
       (TRef ty, type_subst')
   | Deref e ->
       let (ty, type_subst') = infer_type type_ctx type_subst e in
-      begin
-        match ty with
-        | TRef ty -> (ty, type_subst')
-        | _ ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ " : "
-             ^ string_of_typ ty ^ " is not a ref type")
+      begin match ty with
+      | TRef ty -> (ty, type_subst')
+      | _ ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ " : "
+           ^ string_of_typ ty ^ " is not a ref type")
       end
   | Assign (e1, e2) ->
       let (ty1, type_subst') = infer_type type_ctx type_subst e1 in
       let (ty2, type_subst'') = infer_type type_ctx type_subst' e2 in
-      begin
-        match mgu_type (Type_ctx.get_type_env type_ctx) (ty1, TRef ty2) with
-        | Some type_subst''' ->
-            Util.Debug.print_debug
-              ("We get an assign with " ^ Syntax.string_of_term e1 ^ " of type "
-             ^ Types.string_of_typ ty1 ^ " and " ^ Syntax.string_of_term e2
-             ^ " of type " ^ Types.string_of_typ ty2);
-            let type_subst_final =
-              compose_type_subst type_subst''' type_subst'' in
-            Util.Debug.print_debug @@ "The current type substitution is : "
-            ^ string_of_type_subst type_subst_final;
-            (TUnit, type_subst_final)
-        | None ->
-            Util.Error.fail_error @@ "Error typing "
-            ^ Syntax.string_of_term expr ^ " : " ^ string_of_typ ty1
-            ^ " is not unifiable with ref " ^ string_of_typ ty2
+      begin match mgu_type (Type_ctx.get_type_env type_ctx) (ty1, TRef ty2) with
+      | Some type_subst''' ->
+          Util.Debug.print_debug
+            ("We get an assign with " ^ Syntax.string_of_term e1 ^ " of type "
+           ^ Types.string_of_typ ty1 ^ " and " ^ Syntax.string_of_term e2
+           ^ " of type " ^ Types.string_of_typ ty2);
+          let type_subst_final = compose_type_subst type_subst''' type_subst'' in
+          Util.Debug.print_debug @@ "The current type substitution is : "
+          ^ string_of_type_subst type_subst_final;
+          (TUnit, type_subst_final)
+      | None ->
+          Util.Error.fail_error @@ "Error typing " ^ Syntax.string_of_term expr
+          ^ " : " ^ string_of_typ ty1 ^ " is not unifiable with ref "
+          ^ string_of_typ ty2
       end
   | Assert e ->
-      let (ty, type_subst') = infer_type type_ctx type_subst e in
-      begin
-        match ty with
-        | TBool -> (TUnit, type_subst')
-        | _ ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ " : "
-             ^ string_of_typ ty ^ " is not equal to bool.")
-      end
+      let type_subst' = check_type type_ctx type_subst e TBool in
+      (TUnit, type_subst')
   | Raise e ->
       let (ty, type_subst') = infer_type type_ctx type_subst e in
-      begin
-        match ty with
-        | TExn ->
-            let tvar = fresh_typevar () in
-            (tvar, type_subst')
-        | _ ->
-            Util.Error.fail_error
-              ("Error typing " ^ Syntax.string_of_term expr ^ " : "
-             ^ string_of_typ ty ^ " is not equal to exn.")
+      begin match ty with
+      | TExn ->
+          let tvar = fresh_typevar () in
+          (tvar, type_subst')
+      | _ ->
+          Util.Error.fail_error
+            ("Error typing " ^ Syntax.string_of_term expr ^ " : "
+           ^ string_of_typ ty ^ " is not equal to exn.")
       end
   | TryWith (e, handler_l) ->
       let (ty, type_subst') = infer_type type_ctx type_subst e in
       let aux (ty, type_subst) (Handler (pat, e_handler)) =
         let (ty', type_subst') =
-          begin
-            match pat with
-            | PatCons _ ->
-                (* TODO: We should check that the constructor exists *)
-                infer_type type_ctx type_subst e_handler
-            | PatVar id ->
-                let type_ctx' = Type_ctx.extend_var_ctx type_ctx id TExn in
-                infer_type type_ctx' type_subst e_handler
+          begin match pat with
+          | PatCons _ ->
+              (* TODO: We should check that the constructor exists *)
+              infer_type type_ctx type_subst e_handler
+          | PatVar id ->
+              let type_ctx' = Type_ctx.extend_var_ctx type_ctx id TExn in
+              infer_type type_ctx' type_subst e_handler
           end in
-        begin
-          match mgu_type (Type_ctx.get_type_env type_ctx) (ty, ty') with
-          | Some type_subst'' ->
-              (ty, compose_type_subst type_subst'' type_subst')
-          | None -> failwith "Type checking of try with not fully implemented."
+        begin match mgu_type (Type_ctx.get_type_env type_ctx) (ty, ty') with
+        | Some type_subst'' -> (ty, compose_type_subst type_subst'' type_subst')
+        | None -> failwith "Type checking of try with not fully implemented."
         end in
       List.fold_left aux (ty, type_subst') handler_l
   | Hole -> failwith "Error: The typechecker cannot type a hole."
